2da4cdfde7d5f851a43d3a3566241e2767da7c1f
pbc1001 <mintcho0328@gmail.com>
2026-02-04 22:11:10 +0900

    chore: setting project(#2)

---
+++ Simvex_FE/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+// import PostList from "./pages/PostList"; 예시
+
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/lost" element={<LostList />} /> 예시 */}
+    </Routes>
+  );
+}
+
+export default App;
