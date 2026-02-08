13f44fb7b66c3e93a52228d13d4f67518c335ae5
HEEWON0010 <chw001025@gmail.com>
2026-02-09 00:22:54 +0900

    feat: home 페이지 퍼블리싱(#11)

--- Simvex_FE/src/App.jsx
+++ Simvex_FE/src/App.jsx
@@ -1,10 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-import CAD from "./pages/cad/App";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import ObjectSelect from './pages/ObjectSelect';
+import Study from './pages/study';
+import ProjectSelect from './pages/ProjectSelect';
+import CAD from './pages/cad/App';
+import './styles/global.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<CAD />} />
+      <Route path="/" element={<Home />} />
+      
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      
+      <Route path="/study/select" element={<ObjectSelect />} />
+      <Route path="/study" element={<Study />} />
+      
+      <Route path="/cad/select" element={<ProjectSelect />} />
       <Route path="/cad" element={<CAD />} />
     </Routes>
   );
